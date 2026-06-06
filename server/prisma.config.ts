77ce545daf138c53d9e9dbaf4a6d995487ecd31f
Mike47ip <agyemankojo47@gmail.com>
2026-06-06 19:03:16 +0100

    Add prisma.config.ts

---
+++ server/prisma.config.ts
@@ -0,0 +1,11 @@
+// server/prisma.config.ts
+
+import { defineConfig } from "prisma/config";
+import "dotenv/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  datasource: {
+    url: process.env.DATABASE_URL,
+  },
+});
